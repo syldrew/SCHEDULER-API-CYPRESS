f2a6a0be7f82f3e041615311916d0c01b6e49221
syldrew <syl_peke14@hotmail.com>
2023-10-03 14:52:51 -0500

    FIRST COMMIT

---
+++ scheduler/src/hooks/useVisualMode.js
@@ -0,0 +1,28 @@
+import {useState} from 'react';
+
+
+export default function useVisualMode(initial, newMode) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition (newMode, replace = false) {
+    if (replace) {
+      setMode(newMode);
+      setHistory(prev => [...prev].slice(0, prev.length -1),newMode);
+    }
+    setMode(newMode);
+    setHistory(prev => [...prev, newMode]);
+  };
+
+  function back () {
+    if (history.length > 1) {
+      setHistory(prev => [...prev.slice(0, prev.length - 1)]);
+    }
+  };
+
+  return { mode: history[history.length - 1], transition, back };
+};
+  
+  
+  
+  
